7593f257e6eb37ff19be0b0ca928638afd5e430d
anyrandom <140530383+anyrandom@users.noreply.github.com>
2023-08-29 21:35:20 +0000

    Update keurig_barista.py

--- keurig_barista.py
+++ keurig_barista.py
@@ -38,7 +38,9 @@ st.markdown(
    .stChatMessage [data-testid="chatAvatarIcon-assistant"],.stChatMessage img{width:40px; height:40px; border-radius:50%;}
    .stMarkdown .stMarkdownContainer{ background-color:#fff;
    padding:10px 10px ; font-size:14px; color:#212120;
-   border-radius:8px} 
+   border-radius:8px}
+   section[data-testid="stSidebar"] .css-ng1t4o {{width: 25rem;}}
+   section[data-testid="stSidebar"] .css-1d391kg {{width: 25rem;}}
    .stChatFloatingInputContainer{padding-bottom:20px;}
    .stChatInputContainer{ background-color:#fff; color:#212120; }
    .stChatInputContainer textarea{color:#212120 !important;padding-left:10px !important;
