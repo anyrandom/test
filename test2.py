b179fa30db3c59f30fde84028ba7b54484a66ccb
anyrandom <140530383+anyrandom@users.noreply.github.com>
2023-09-26 23:18:26 +0000

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,15 @@
 import streamlit as st
-from st_audiorec import st_audiorec
+from audiorecorder import audiorecorder
 
-wav_audio_data = st_audiorec()
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Click to stop recording")
 
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
+if not audio.empty():
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
+
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
+
+    # To get audio properties, use pydub AudioSegment properties:
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
