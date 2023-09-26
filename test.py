7ce7dc8cad6423c05f639e7023d15863f4d304ed
anyrandom <140530383+anyrandom@users.noreply.github.com>
2023-09-26 22:48:55 +0000

    Adding test files

---
+++ test.py
@@ -0,0 +1,309 @@
+import streamlit as st
+import random
+import os
+import openai
+from st_audiorec import st_audiorec
+from audiorecorder import audiorecorder
+import speech_recognition as sr
+from PIL import Image
+
+
+k_small_logo = Image.open("keurig_small_logo.png")
+k_logo = Image.open("keurig_logo.png")
+barista_logo = Image.open("barista-chat-icon.png")
+
+raw_sidebar_image = Image.open("barista_large.png")
+barista_large = raw_sidebar_image.resize((220,330))
+
+st.set_page_config(page_title="Keurig Barista", page_icon=k_small_logo, initial_sidebar_state="auto", layout="wide")
+st.sidebar.image(barista_large)
+st.sidebar.title("Welcome To Keurig Virtual Personal Barista \n Let me help you find the perfect beverage!")
+# st.sidebar.divider()
+#st.sidebar.subheader("Let me help you find the perfect beverage!")
+# #mood_option = st.sidebar.selectbox('How are you feeling today?',('Happy', 'Tired', 'Gloomy', 'Sick'))
+
+openai.api_type = "azure"
+openai.api_base = "https://testaisvc.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = "1164d7a0490a41b9b6ec3a32d4c77b5a"
+
+st.markdown(
+    """
+    <style>
+    section[data-testid="stSidebar"] {width: 550px !important;}
+    [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+    .reportview-container ,.appview-container,.stChatFloatingInputContainer{
+        background-color: #EDEEF2;
+    }
+    .embeddedAppMetaInfoBar_container__DxxL1{background-color: #fff !important;height:100px!important}
+   .stChatMessage{background-color:none; padding:0px 10px;}
+   .stChatMessage [data-testid="chatAvatarIcon-user"]{display:none;}
+   .stChatMessage [data-testid="chatAvatarIcon-user"] ~ div{margin-left:80px;}
+   .stChatMessage .stMarkdown{background-color:#fff;
+   padding:10px 10px ; font-size:14px; color:#212120;
+   border-radius:8px}
+   .stChatMessage [data-testid="chatAvatarIcon-user"] ~ div .stMarkdown{background:#ECE3FE !important;}
+   .stChatMessage [data-testid="chatAvatarIcon-assistant"] ~ div .stMarkdown {background:#fff !important;}
+   .stChatMessage [data-testid="chatAvatarIcon-assistant"],.stChatMessage img{width:40px; height:40px; border-radius:50%;}
+   .stMarkdown .stMarkdownContainer{ background-color:#fff;
+   padding:10px 10px ; font-size:14px; color:#212120;
+   border-radius:8px}
+   section[data-testid="stSidebar"] {width: 600px !important;}
+   .stChatFloatingInputContainer{padding-bottom:20px;}
+   .stChatInputContainer{ background-color:#fff; color:#212120; }
+   .stChatInputContainer textarea{color:#212120 !important;padding-left:10px !important;
+     background-image: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7") }
+   .stChatInputContainer .st-bw{border:1px solid #C5C5C5 !important;}
+   .stChatInputContainer .st-bw textarea{border:0px !important;}
+   .stChatInputContainer button:hover{background:none !important;color:#212120;}
+   .stChatInputContainer button svg{transform: rotate(-30deg);color:#212120;top: -3px;
+position: relative;}
+   
+   .stChatInputContainer textarea::-webkit-input-placeholder {
+            color:#212120;
+        }
+    .stChatInputContainer textarea:-moz-placeholder {
+             color:#212120;
+        }
+      .stChatInputContainer textarea::-moz-placeholder {
+             color:#212120;
+        }
+     
+       .stChatInputContainer textarea:-ms-input-placeholder {
+             color:#212120;
+        }
+     
+       .stChatInputContainer textarea::placeholder {
+             color:#212120;
+        }   
+   .sidebar .sidebar-content {
+        background: #fff
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+conversation = [
+
+    {"role": "system", "content": "You are a Keurig AI assistant built to help people find coffee or tea or dessert recipes from the brand "
+                                  "Keurig only. If asked questions about topics other than coffee or tea or keurig recipes, answer that "
+                                  "you are a virtual assistant built for Keurig, and are unable to answer such questions about other topics."
+                                  "Do not answer any questions unrelated to coffee or tea or desserts, and mention that you are "
+                                  "a virtual assistant built for coffee and beverage recommendations only. "
+                                  "Do not answer questions about competitor coffee brands, like Starbucks or Nescafe."},
+    {"role": "user", "content": "what are the coffee types?"},
+    {"role": "assistant", "content": "There are various types of coffee, including hot coffees, iced coffees, "
+                                     "hot teas, iced teas, and more. Some examples of hot coffees are Caribou Coffee "
+                                     "S'mores Cabin Latte, and Lavender Latte. Some examples of iced coffees are "
+                                     " McCafe Iced One Step Mocha "
+                                     "Frappe and Iced Caramel Cookie Coffee. Hot tea options include Mother Rose "
+                                     "Best and English Breakfast Latte. Iced tea options include Iced Bubble Tea Latte."
+                                     " Additionally, there are various desserts and more, such as Athletic Brew Co "
+                                     "Free Wave Hazy IPA Iced Coffee and Swiss Miss S'mores Hot Cocoa."},
+    {"role": "user", "content": "what type of caramel coffees are there?"},
+    {"role": "assistant", "content": "There are several types of caramel coffees available, including Caramel Cookie "
+                                     "Coffee, Caramel Macchiato, and Iced Caramel Cookie Coffee."},
+    {"role": "user", "content": "what are different Cappuccino I can try?"},
+    {"role": "assistant", "content": "There are several types of Cappuccino available, including Cappuccino, Collagen "
+                                     "Cappuccino, Lite Cappuccino, Jason Oppenheim’s Maple Cinnamon Cappuccino, "
+                                     "Iced Cappuccino, Iced Collagen Cappuccino, and Lite Iced Cappuccino"},
+    {"role": "user", "content": "what are different Vanilla Flavored drinks I can try"},
+    {"role": "assistant", "content": "There are several Vanilla Flavored Drinks for you to try like Vanilla Latte, "
+                                     "Vanilla Brownie Coffee, Iced Vanilla Brownie Coffee, Dr Pepper® Vanilla Caramel "
+                                     "Iced Coffee, Vanilla Caramel Coffee Banana Smoothie, and Strawberry Vanilla "
+                                     "Coffee Parfait"},
+    {"role": "user", "content": "I am in the mood for some latte"},
+    {"role": "assistant", "content": "Keurig has a wide variety of latte drinks to choose from incuding Caribou "
+                                     "Coffee® S’mores Cabin Latte, McCafe® Horchata Latte, Lavender Latte, Latte, "
+                                     "Mocha Hazelnut Latte, Vanilla Latte, Luck of the "
+                                     "Latte, “Be Mine” Mocha Latte, "
+                                     "English Breakfast Latte, and Iced Bubble Tea Latte, or check out more recipes"
+                                     " on our [Recipes](https://www.keurig.com/hub/recipes) page."},
+    {"role": "user", "content": "Suggest some Macchiato Drinks"},
+    {"role": "assistant", "content": "There are several Macchiato Drinks like Caramel Macchiato, Macchiato, "
+                                     "Lite Macchiato, and Lite Iced Macchiato"},
+    {"role": "user", "content": "What fruit drinks options are available?"},
+    {"role": "assistant", "content": "Keurig mainly offers coffee, tea, and hot cocoa products. But there are also "
+                                     "fruit-based drinks for users to enjoy. You can select drinks like Strawberry "
+                                     "Vanilla Coffee Parfait, Cherry Bourbon Mash, Vanilla Caramel Coffee Banana "
+                                     "Smoothie, Orange You Glad It’s Coffee, Coconut Mocha Coffee Cocktail, "
+                                     "or check out more recipes on our [Recipes](https://www.keurig.com/hub/recipes) page."},
+    {"role": "user", "content": "what are the different Frappe drinks to try"},
+    {"role": "assistant", "content": "Keurig offers McCafe® Iced One Step Mocha Frappe to enjoy a flavourful frappe "
+                                     "drink."},
+    {"role": "user", "content": "What are the different Hot Coffees?"},
+    {"role": "assistant", "content": "There is a wide range of Hot Coffees from Keurig for you to choose. You can "
+                                     "select McCafe® Horchata Latte, "
+                                     "Mocha Hazelnut Latte, Caramel Cookie Coffee, Caramel Macchiato, "
+                                     "Cafe Mocha, Collagen Cappuccino, Lite Cappuccino, Cappuccino,"
+                                     "or check out recipes from a wide range of options on our "
+                                     "[Hot Coffees selection page]("
+                                     "https://www.keurig.com/hub/recipes?category=hot-coffees#cards-container)."},
+    {"role": "user", "content": "hot coffees"},
+    {"role": "assistant", "content": "There is a wide range of Hot Coffees from Keurig for you to choose. You can "
+                                     "select McCafe® Horchata Latte, "
+                                     "Mocha Hazelnut Latte, Caramel Cookie Coffee, Caramel Macchiato, "
+                                     "Cafe Mocha, Collagen Cappuccino, Lite Cappuccino, Cappuccino,"
+                                     "or check out recipes from a wide range of options on our "
+                                     "[Hot Coffees selection page]("
+                                     "https://www.keurig.com/hub/recipes?category=hot-coffees#cards-container)."},
+    {"role": "user", "content": "What are the different Iced Coffees?"},
+    {"role": "assistant", "content": "Keurig has plenty of offerings for a cup of Iced Coffee to make which includes "
+                                     "McCafe® Iced One Step Mocha Frappe, Iced Caramel Cookie Coffee, "
+                                     "Iced Vanilla Brownie Coffee, Iced Oatmeal Cookie Coffee,"
+                                     "Sparkling Iced Coffee, or check out recipes from a wide range of"
+                                     "options on our [Iced Coffees selection page]("
+                                     "https://www.keurig.com/hub/recipes?category=iced-coffees#cards-container)."},
+    {"role": "user", "content": "iced coffees"},
+    {"role": "assistant", "content": "Keurig has plenty of offerings for a cup of Iced Coffee to make which includes "
+                                     "McCafe® Iced One Step Mocha Frappe, Iced Caramel Cookie Coffee, "
+                                     "Iced Vanilla Brownie Coffee, Iced Oatmeal Cookie Coffee,"
+                                     "Sparkling Iced Coffee, or check out recipes from a wide range of"
+                                     "options on our [Iced Coffees selection page]("
+                                     "https://www.keurig.com/hub/recipes?category=iced-coffees#cards-container)."},
+    {"role": "user", "content": "What are the different desserts?"},
+    {"role": "assistant", "content": "Keurig provides a selection from desserts apart from Coffees and Tea for "
+                                     "customers to enjoy. You can make Mule Love It, Vanilla Caramel Coffee Banana Smoothie, "
+                                     "Cherry Bourbon Mash, Espresso Style Martini, Coconut Mocha Coffee Cocktail, "
+                                     "Swiss Miss® S’mores Hot Cocoa, or check out recipes from a wide range of"
+                                     "options on our "
+                                     "[Desserts & More](https://www.keurig.com/hub/recipes?cm_sp=desserts+smoothies-_-Top-Nav-_-recipes101&category=desserts-and-smoothies#cards-container) page."},
+    {"role": "user", "content": "What are the different Hot Teas?"},
+    {"role": "assistant", "content": "You can make refreshing teas such as Mother Rose Best, English Breakfast Latte"},
+    {"role": "user", "content": "What are the different Iced Teas"},
+    {"role": "assistant", "content": "You can make Iced Bubble Tea Latte with Keurig."},
+    {"role": "user", "content": "What are the currently trending recipes?"},
+    {"role": "assistant", "content": "The Keurig community keeps making delicious and easy to make drinks all the "
+                                     "time. Our list is constantly changing, but currently the trending recipes "
+                                     "include Jason Oppenheim’s Maple Cinnamon Cappuccino, Espresso Style Martini, "
+                                     "Coconut Mocha Coffee Cocktail, Chewy Coconut Mocha Granola Bars, You're On "
+                                     "Fire, and Sundae Fundae Cocktail"},
+    {"role": "user", "content": "I have a Headache, can you suggest some drinks"},
+    {"role": "assistant", "content": "I'm sorry to hear that you have a headache. For headaches, you can try our "
+                                     "Mother Rose Best Tea or English Breakfast Tea. But you should seek medical help "
+                                     "for better treatment and ideal solution."},
+    {"role": "user", "content": "Which coffee should I have to have a fresh start to the day?"},
+    {"role": "assistant", "content": "We have a wide range of coffees you can select. Try out Cappuccino, Collagen "
+                                     "Cappuccino, Jason Oppenheim’s Maple Cinnamon Cappuccino, or Caramel Macchiato."},
+    {"role": "user", "content": "Which coffee should I make for a date?"},
+    {"role": "assistant", "content": "For you and your loved one, you can try out “Be Mine” Mocha Latte, McCafe® Iced "
+                                     "One Step Mocha Frappe, Iced Vanilla Brownie Coffee, Caramel Cookie Coffee, "
+                                     "National Donut Day Iced Coffee, Vanilla Brownie Coffee or check out recipes "
+                                     "from a wide range of options on our [Recipes]("
+                                     "https://www.keurig.com/hub/recipes) page."},
+    {"role": "user", "content": "Coffee for fresh start on a hot sunny morning"},
+    {"role": "assistant", "content": "For a hot sunny morning like today, I would recommend Iced Coffee, "
+                                     "Iced Cappuccino, Morning Coffee Shot, Lite Iced Cappuccino, Lite Iced Latte, "
+                                     "or a Lite Iced Macchiato to have a fresh start to the day."},
+    {"role": "user", "content": "I am in a sad or gloomy mood"},
+    {"role": "assistant", "content": "I am sorry to hear you are feeling sad. A comforting coffee can lift anyone's "
+                                     "mood. Why don't you try Iced Caramel Cookie Coffee, Iced Vanilla Brownie "
+                                     "Coffee, Cafe Mocha, Vanilla Brownie Coffee, “Be Mine” Mocha Latte "
+                                     "or McCafe® Iced One Step Hazelnut Latte. Remember, it's important to take care "
+                                     "of yourself and seek help if you need it."},
+    {"role": "user", "content": "Coffee for girlfriend"},
+    {"role": "assistant", "content": "We have a wide range of coffees you can make to make your loved one happy. How "
+                                     "about McCafe® Iced One Step Hazelnut Latte, "
+                                     "Iced Caramel Cookie Coffee, Mocha Hazelnut "
+                                     "Latte, McCafe® Café Con Leche, or a Vanilla Brownie Coffee. These will surely "
+                                     "give you some brownie points!!"},
+    {"role": "user", "content": "What is the weather like"},
+    {"role": "assistant", "content": "I'm sorry, but as a Keurig AI assistant, I am built to help people find "
+                                     "coffee or tea or dessert recipes from the brand Keurig only. "
+                                     "I do not have access to real-time weather information. "
+                                     "However, you can check the weather forecast on your local news website or a weather app on your phone."}
+
+
+]
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you? To get started, "
+                                                                  "I can recommend a beverage based on your mood, "
+                                                                  "or you can ask for a category of beverage you are interested in."}]
+
+
+for message in st.session_state.messages:
+    if message["role"] == "assistant":
+        with st.chat_message(message["role"], avatar=barista_logo):
+            st.write(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+# enable_user_voice_search = False
+# with st.sidebar:
+#     if st.button("Voice Search", key="voice_search"):
+#         enable_user_voice_search = True
+#
+# if enable_user_voice_search:
+#     voice_input = handle_voice_input()
+#     prompt = st.chat_input(voice_input)
+# else:
+#     prompt = st.chat_input("Say something")
+
+def handle_voice_input(audio_input):
+    r = sr.Recognizer()
+    # container = st.empty()
+    # container.write("Recording started")
+    # with sr.Microphone() as source:
+    #     audio = r.listen(source, timeout=5)
+    #     container.write("Recording complete")
+    try:
+        audio_text = r.recognize_google(audio_input)
+        # container.empty()
+        return audio_text
+    except Exception as e:
+        #container.empty()
+        return "Say something" #None
+
+audio = audiorecorder("Click to record", "Click to stop recording")
+if not audio.empty():
+    # To play audio in frontend:
+    #st.audio(audio.export().read())
+    voice_input = handle_voice_input(audio)
+    prompt = st.chat_input(voice_input)
+    #
+    # # To save audio to a file, use pydub export method:
+    # audio.export("audio.wav", format="wav")
+    # # To get audio properties, use pydub AudioSegment properties:
+    # st.write(f"Frame rate: {audio.frame_rate}, "
+    #          f"Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+else:
+    prompt = st.chat_input("Say something")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    conversation.append({"role": "user", "content": prompt})
+
+    thinking_msg = st.empty()
+    thinking_msg.text("Brewing...")
+
+    completion = openai.ChatCompletion.create(
+        engine="gpt-35-turbo",
+        messages=conversation,
+        temperature=0,
+        max_tokens=800,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    chat_response = completion.choices[0].message.content
+
+    thinking_msg.empty()
+    with st.chat_message("Assistant", avatar=barista_logo):
+        st.write(chat_response)
+
+    message = {"role": "assistant", "content": chat_response}
+    conversation.append(message)
+    st.session_state.messages.append(message)
